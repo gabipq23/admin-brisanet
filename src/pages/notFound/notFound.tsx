ee6ade92bc5ec3a874dfd3bfdcb371ee04d9ba3c
Gabriela Quintaes <gabrielapquintaes@gmail.com>
2026-03-18 09:47:32 -0300

    fix: adjust logo size in NotFound component

--- src/pages/notFound/notFound.tsx
+++ src/pages/notFound/notFound.tsx
@@ -20,7 +20,7 @@ export default function NotFound() {
     <div className="h-[601px] bg-red-white flex justify-center items-center">
       <div className="flex h-[220px] w-[300px] bg-[#c5c5c5] p-8 flex-col rounded shadow-2xl items-center justify-center  text-[#ff4800] gap-4">
         <img
-          className="h-16 w-16"
+          className="h-8"
           src="/assets/brisanet.png"
         // style={{
         //   filter:
